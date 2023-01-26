439e1ed14edbb7e443da79863081e49c6f2c133f
ai_nick <alittlejoke@mail.ru>
2023-01-26 11:22:53 +0300

    first ex

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два 
+числа (A и B) и возводит число A в натуральную степень B. 
+Задание должно быть выполнено в методе. В методе не должно 
+быть вывода в консоль.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int a);
+bool isParsed1 = int.TryParse(Console.ReadLine(), out int b);
+
+int noun = GetNounNumbers(a);
+
+Console.WriteLine(noun);
+
+int GetNounNumbers(int b)
+{
+    int noun = a;
+
+    for (int i = 0; i <= b; i++)
+    {
+        noun *= a;
+    }
+    return noun;
+}
